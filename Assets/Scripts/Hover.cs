5e7ef0ace3566bf7c1dfc8c873e2ab8467329cbf
IAmTheRedSpy <djacoby@shaw.ca>
2020-02-01 23:55:25 -0800

    add main menu

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.gray;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
