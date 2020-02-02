b871a8d73513aa5a55f7b6ab2a177b9a6d8ae1be
IAmTheRedSpy <djacoby@shaw.ca>
2020-02-01 18:07:32 -0800

    finished parts ui

---
+++ Assets/Scripts/Parts.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parts : MonoBehaviour
+{
+    public static int numCollected;
+    public static int totalNum;
+    private void Start()
+    {
+        numCollected = 0;
+        totalNum = 6;
+    }
+}
---
+++ Assets/Scripts/UIUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIUpdate : MonoBehaviour
+{
+
+    private UnityEngine.UI.Text partsText;
+    private string staticPartsUI = "/6 parts found";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        partsText = GameObject.Find("UIParts").GetComponent<UnityEngine.UI.Text>();
+        staticPartsUI = "/" + Parts.totalNum + " parts found";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int numPartsFound = Parts.numCollected;
+        partsText.text = numPartsFound + staticPartsUI;
+    }
+}
